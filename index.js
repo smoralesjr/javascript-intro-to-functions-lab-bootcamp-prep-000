aac4df1451013731eed07f4bbfc5b0c22faea573
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-25 15:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ const sayHiToGrandma = function (string){
   let uppercase = string.toUpperCase();
   
   if (string === lowercase){
+    
     return "I can't hear you!";
+    
   } else if (string === uppercase){
+    
     return "YES INDEED!";
+    
   } else if (string === "I love you, Grandma.") {
+    
     return "I love you, too.";
+    
   }
+  
 };
